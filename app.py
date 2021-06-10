3c0fce261e5f6747845987c406d6ec91be3f4e80
paulvmking <paulvmking@gmail.com>
2021-06-10 00:49:09 +0000

    moved buttons to recipe page

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def add_recipe():
     return render_template("add_recipe.html", categories=categories)
 
 
-@app.route("/edit_task/<recipe_id>", methods=["GET", "POST"])
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
