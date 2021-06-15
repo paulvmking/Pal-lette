40e0a81e3ceb5e46c4ed4cd5806e29adb5233a87
paulvmking <paulvmking@gmail.com>
2021-06-15 13:09:23 +0000

    Created add category functionality for admin

--- app.py
+++ app.py
@@ -181,6 +181,19 @@ def get_categories():
     return render_template("categories.html", categories=categories)
 
 
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.insert_one(category)
+        flash("New Category Added")
+        return redirect(url_for("get_categories"))
+
+    return render_template("add_category.html")
+
+
 @app.route("/view_recipe/<recipe_id>", methods=["GET", "POST"])
 def view_recipe(recipe_id):
 
