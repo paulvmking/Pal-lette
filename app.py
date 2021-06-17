f930ec53f9c250c2c83d485ae2c5e89e29d37896
paulvmking <paulvmking@gmail.com>
2021-06-17 22:14:48 +0000

    Changed debug to False

--- app.py
+++ app.py
@@ -354,4 +354,4 @@ def remove_recipe(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
